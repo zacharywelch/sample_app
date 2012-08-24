5eec8de23bf3e9df34b782535cc9107558566527
Zachary <zwelch154@gmail.com>
2012-08-23 20:43:48 -0400

    fill in layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,6 +10,12 @@ describe "Static pages" do
     	page.should have_selector('h1', :text => 'Sample App')
     end
 
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', 
+        :text => 'Ruby on Rails Tutorial Sample App')
+    end
+
     it "should have the right title" do
   		visit '/static_pages/home'
   		page.should have_selector('title',
