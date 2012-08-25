98014ea8b971da423669971b7e0095b836c7b2f9
Zachary <zwelch154@gmail.com>
2012-08-25 17:17:01 -0400

    deleted pasword_digest migration

--- db/migrate/20120825050235_add_password_digest_to_users.rb
+++ db/migrate/20120825050235_add_password_digest_to_users.rb
@@ -1,6 +1,5 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration
   def change
     add_column :users, :password_digest, :string
-    remove_column :users, :pasword_digest, :string
   end
 end
